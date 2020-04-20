fc8ce93430907caea8121f20f18491715e9b68f4
unknown <francofourmantin@gmail.com>
2020-04-20 13:16:06 -0300

    Pequeño fix a handle_functions.js

--- Chally/public/js/handle_functions.js
+++ Chally/public/js/handle_functions.js
@@ -1,19 +1,21 @@
 let allLinks = document.querySelectorAll('a');
 allLinks.forEach(a => a.addEventListener('click' , function(e){
+    if(!linkActual.search('google') || !linkActual.search('facebook')){
     e.preventDefault();
     let linkActual = a.href;
-    fetch(linkActual)
-    .then(function(response){
-        return response.text();
-    })
-    .then(function(data){
-        if(data === '"desautorizado"'){
-            $('#modal-registro').modal('show');
-        }else{
-            window.location.href = linkActual;
-        }
-    })
-    .then(function(error){
-        console.log(error);
-    })
+        fetch(linkActual)
+        .then(function(response){
+            return response.text();
+        })
+        .then(function(data){
+            if(data === '"desautorizado"'){
+                $('#modal-registro').modal('show');
+            }else{
+                window.location.href = linkActual;
+            }
+        })
+        .then(function(error){
+            console.log(error);
+        })
+    }
 }));
